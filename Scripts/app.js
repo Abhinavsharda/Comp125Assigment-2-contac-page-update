53852687db872f18d653293101d81fd8c5e8025e
asharda1 <300868625@cencol.ca>
2016-06-10 17:38:22 -0400

    FINNAL COMMIT

--- Scripts/app.js
+++ Scripts/app.js
@@ -9,34 +9,6 @@
 * @description This contain javascript 
 */ 
   //create  a link for sending form
-   var sendButton = document.getElementById("sendButton");
-   
-   sendButton.addEventListener("click", sendButtonClick);
-   
-   function sendButtonClick(event) {
-       console.log("pressed!");
-   }
-   // creating link to all the field
-   var fullName = document.getElementById("fullName");
-   var email = document.getElementById("Email");
-   var cNumber = document.getElementById("ContactNumber");
-   var message = document.getElementById("Message");
-
-   //a refrence to form
-   var contactmeform = document.getElementById("contactmeform");
-    
-    contactmeform.addEventListener("submit", function(event){
-        event.preventDefault();
-        console.log("posted");
-        console.log("Full Name: " + fullName.value);
-        console.log("Email: " + email.value);
-        console.log("Contact Number: " + cNumber.value);
-        console.log("Message: " + message.value);
-        contactmeform.reset();
-    });
-    
-
-    
 var write = [
     "Abhinav Sharda",
     "I was born in India on may 07,1997. I was born and raised in middle class family.our family has great affection with animals so we are always ready help any needy animals. I persuade all my studies in CBSE .having agreat intrest in computers made me to decide to came canada for my higher qualification in the technology field. I hope to get a good job in this field.",
@@ -61,9 +33,35 @@ var write = [
     keys[6] = document.getElementById('about2');
     keys[7] = document.getElementById('projects3');
     keys[8] = document.getElementById('about3');
-    
+   
     for (var i = 0; i < keys.length; i++) {
         
         if (keys[i]){
             keys[i].textContent = write[i];}}  
 })();
+
+   var sendButton = document.getElementById("sendButton");
+   
+    sendButton.addEventListener("click", sendButtonClick);
+   
+   function sendButtonClick(event) {
+       console.log("clicked!");
+   }
+   // creating link to all the field
+   var fullName = document.getElementById("fullName");
+   var email = document.getElementById("Email");
+   var cNumber = document.getElementById("ContactNumber");
+   var message = document.getElementById("Message");
+
+   //a refrence to form
+   var contactmeform = document.getElementById("contactmeform");
+    
+    contactmeform.addEventListener("submit", function(event){
+        event.preventDefault();
+        console.log("posted");
+        console.log("Full Name: " + fullName.value);
+        console.log("Email: " + email.value);
+        console.log("Contact Number: " + cNumber.value);
+        console.log("Message: " + message.value);
+        contactmeform.reset();
+    });
